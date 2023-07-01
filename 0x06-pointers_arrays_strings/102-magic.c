5e5ee120aeed53d09497073d7ba673ea38d45400
khadija199822 <khadija98benchalouh@gmail.com>
2023-06-30 17:28:55 -0700

    g

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -10,13 +10,15 @@ int main(void)
     p = &n;
 
     /*
-     * Corrected line of code:
-     * Assign the value 98 to the memory location that is 3 integers ahead of the address stored in p
-     * (assuming the size of int is 4 bytes on your system)
+     * write your line of code here...
+     * Remember:
+     * - you are not allowed to use a
+     * - you are not allowed to modify p
+     * - only one statement
+     * - you are not allowed to code anything else than this line of code
      */
-    *(p + 3) = 98;
-
+    *(p + 5) = 98;
     /* ...so that this prints 98\n */
     printf("a[2] = %d\n", a[2]);
-    return 0;
+    return (0);
 }
